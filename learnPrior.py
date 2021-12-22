3e8c9430291798dc5918df3604c20bdc9fe2e6be
MathisClautrier <mclautrier98@gmail.com>
2021-12-22 11:04:50 +0100

    improving prior skill learning

--- learnPrior.py
+++ learnPrior.py
@@ -14,16 +14,16 @@ from os.path import isfile, join
 
 parser = argparse.ArgumentParser(description='prior_training')
 parser.add_argument('--env-name', type=str, default='Maze-Medium-v0', help='environment ID')
-parser.add_argument('--simple-like',  action='store_true', help='Initiate the environment with shortest path similar to the possible ones in SimpleMaze')
 parser.add_argument('--seed', type = int, default = 0, help = 'random seed')
 parser.add_argument('--embedding-dim', type = int, default = 10, help = 'embedding dimension')
 parser.add_argument('--H', type = int, default = 10, help = 'trajectory size')
-parser.add_argument('--data-dir', type = str, default = 'not_provided',help='direction to the data')
+parser.add_argument('--data-dir', type = str, default = 'not_provided',help='direction to the data must contain a training folder and a validation folder')
 parser.add_argument('--archi',type = str, default = 'pointnet', help = 'backbone of the state encoder')
 parser.add_argument('--hidden-dim',type = int, default = 256)
 parser.add_argument('--hidden-dim-lstm',type = int, default = 128)
 parser.add_argument('--n-layers', type = int, default =3)
-parser.add_argument('--log-dir', type = str, default ='pointnet_3_seed_0')
+parser.add_argument('--log-dir', type = str, default ='.')
+parser.add_argument('--log-name', type = str, default ='pointnet_medium_seed_0')
 parser.add_argument('--batch-size',type = int, default = 16)
 parser.add_argument('--epochs',type = int, default = 1)
 parser.add_argument('--GPU',  action='store_true')
@@ -52,8 +52,9 @@ _, policy_kwargs = get_policy_network(argsdic["archi"], agrsdic["policy_kwargs"]
 
 Enc = PointNetEncoder(**policy_kwargs, embedding = args.embedding_dim)
 
-skillP = SkillPrior(Enc,args.embedding_dim,args.H,args.hidden_dim_lstm)
-skillP.to(device)
+model = SkillPrior(Enc,args.embedding_dim,args.H,args.hidden_dim_lstm)
+model.to(device)
+
 class SkillDataset(torch.utils.data.Dataset):
     def __init__(self, directory,H):
         'Initialization'
@@ -75,26 +76,52 @@ class SkillDataset(torch.utils.data.Dataset):
         A = A.reshape(self.H,2)
         return S,A
 
-dataset = SkillDataset(args.data_dir,args.H)
-training = torch.utils.data.DataLoader(training_set, batch_size = args.batch_size,shuffle = True)
+def log(path, file):
+    log_file = os.path.join(path, file)
 
-optimizer = optim.RAdam(skillP.parameters(), lr=args.lr, betas=[args.beta1,args.beta2])
+    if not os.path.isfile(log_file):
+        open(log_file, "w+").close()
 
-for epoch in range(args.epochs):
+    console_logging_format = "%(message)s"
+    file_logging_format = "%(message)s"
+    logging.basicConfig(level=logging.INFO, format=console_logging_format)
+    logger = logging.getLogger()
+
+    handler = logging.FileHandler(log_file)
+
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(file_logging_format)
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
+
+logger = log(args.log_dir,args.log_name+'.csv')
+datasetT = SkillDataset(args.data_dir+'/training',args.H)
+trainingLoader = torch.utils.data.DataLoader(datasetT, batch_size = args.batch_size,shuffle = True)
+datasetV = SkillDataset(args.data_dir+'/validation',args.H)
+validationLoader = torch.utils.data.DataLoader(datasetV, batch_size = 100)
+
+optimizer = optim.RAdam(model.parameters(), lr=args.lr, betas=[args.beta1,args.beta2])
+
+for epoch in tqdm(range(args.epochs)):
     running_loss=0
     m=0
-    for states,actions in training:
+    model.train()
+    for states,actions in trainingLoader:
         
         optimizer.zero_grad()
-        z_mu,z_var,zs_mu,zs_var,q_z,p_z,pa_z,z,actions_ =  skillP.forward(states.to(device),actions.to(device))
+        z_mu,z_var,zs_mu,zs_var,q_z,p_z,pa_z,z,actions_ =  model.forward(states.to(device),actions.to(device))
         
         loss = torch.square(actions - actions_).sum(axis=1).mean(axis=0).mean()
         
         loss += -args.beta*torch.distributions.kl.kl_divergence(q_z,p_z).sum(axis=1).mean()
         
         with torch.no_grad():
-            q_z_no_grad = skillP.obtain_q_z(actions.to(device))
-        loss += args.beta*torch.distributions.kil.kil_divergence(q_z_no_grad,pa_z)
+            q_z_no_grad = model.obtain_q_z(actions.to(device))
+        loss += torch.distributions.kil.kil_divergence(q_z_no_grad,pa_z)
         
         loss.backward()
         
@@ -102,14 +129,21 @@ for epoch in range(args.epochs):
         
         running_loss += loss.item()
         m+=1
+    model.eval()
+    
+    validation_loss=0
+    k=0
+    for states,actions in validationLoader:
+        z_mu,z_var,zs_mu,zs_var,q_z,p_z,pa_z,z,actions_ =  model.forward(states.to(device),actions.to(device))
         
-    print(running_loss/m)
+        loss = torch.square(actions - actions_).sum(axis=1).mean(axis=0).mean()
+        
+        loss += -args.beta*torch.distributions.kl.kl_divergence(q_z,p_z).sum(axis=1).mean()
+        
+        q_z_no_grad = model.obtain_q_z(actions.to(device))
+        loss += torch.distributions.kil.kil_divergence(q_z_no_grad,pa_z)
+        
+        validation_loss +=loss.item()
+    logger.info(str(epoch)+','+str(training_loss/m)+','+str(validation_loss/k))
 
-
-
-
-
-
-
-
-
+torch.save(model.state_dict(), args.log_dir+'/'+args.log_name)
